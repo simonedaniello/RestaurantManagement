648ddbc9f655acc934d53504d0a0a61d4432edb1
alberto <comes94@gmail.com>
2017-06-05 14:43:11 +0200

    commit

--- create_springuser.sql
+++ create_springuser.sql
@@ -1,3 +1,5 @@
+set character_set_server = 'utf8';
+set collation_server = 'utf8_general_ci';
 create database maindb;
 create user 'isssr'@'localhost' identified by 'password';
 grant all on maindb.* to 'isssr'@'localhost';
