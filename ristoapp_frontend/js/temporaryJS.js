9bedf99f829fbb78d086549b7988afc9af436e91
AntonioFX <antoniofx@hotmail.it>
2017-06-06 22:32:09 +0200

    aggiunto collegamento tra lista dei menu e dettaglio di un menu, trasformazione in pdf e download di un menu

--- ristoapp_frontend/js/temporaryJS.js
+++ ristoapp_frontend/js/temporaryJS.js
@@ -25,40 +25,6 @@ myApp.config(function($routeProvider) {
         });
 });
 
-myApp.service('MenuService', ["$http", function($http) {
-
-    this.getMenu = function(url) {
-        return $http.get(url);
-    };
-}]);
-
-myApp.controller("MenuController", ["$scope", "MenuService", function ($scope, MenuService) {
-
-    $scope.menu = {nomeMenu: undefined, categorie: undefined};
-    //var nomeMenu = $routeParams.nomeMenu;
-
-
-    MenuService.getMenu("jsonFiles/menu_mock.json").then(function (response) {
-        var data = response.data;
-        $scope.menu.nomeMenu = data.nomeMenu;
-        $scope.menu.categorie = data.categorie;
-    }, function (error) {
-        console.log(error);
-    });
-
-
-    /*
-     ajaxService.getResource("../jsonFiles/menu_mock.json", nomeMenu).then(function (response) {
-     var data = response.data;
-     $scope.menu.nomeMenu = data.nomeMenu;
-     $scope.menu.categorie = data.categorie;
-     }, function (error) {
-     console.log(error);
-     });
-     */
-}]);
-
-
 myApp.controller('ajaxController', function($scope, $http) {
 
     /* var recipe = $scope.namerecipe;
