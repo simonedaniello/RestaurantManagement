17b388624dbf0c5632036779ad29b6766dc7282a
auntMario <mariodangelis4@gmail.com>
2017-06-09 21:36:51 +0200

    aggiustato merge sbagliato su creaPietanzaController.js

--- ristoapp_frontend/js/controllers/CreaPietanzaController.js
+++ ristoapp_frontend/js/controllers/CreaPietanzaController.js
@@ -1,4 +1,4 @@
-myApp.controller("CreaPietanzaController", function($scope, ajaxService) {
+myApp.controller("CreaPietanzaController", function($scope, ajaxService, CreaPietanzaService) {
 
     //prodotti e tag dovrebbero essere vuoti e riempiti con una richiesta al server
     $scope.prodotti = [
@@ -11,31 +11,44 @@ myApp.controller("CreaPietanzaController", function($scope, ajaxService) {
         {name:"prosciutto crudo",       id:7},
         {name:"pomodoro",               id:8}
     ];
-    $scope.tags = ["piccante", "alah", "cristiano", "pesce", "carne", "riso"];
 
-    $scope.prova=[]
-    var tagData;
-    ajaxService.getResource("pietanze", tagData).then(function (data) {
-        $scope.prova = data;
-        console.log($scope.prova[0]);
-        console.lo($scope.prova);
-        console.log(tagData);
-    }, function (err) {
-        console.log("Err: " + err);
-    });
+    var updateListaProdotti = function () {
+        ajaxService.getResource("http://localhost:8080/creaPietanza/getProdotti", null).then(
+            function (response) {
+                $scope.tags = CreaPietanzaService.parseProductList(response);
+            }
+            , function (response) {
+                alert("Couldn't get products");
+            });
+    };
+
+    updateListaProdotti();
+
+
+    var updateTagList = function() {
+        ajaxService.getResource("http://localhost:8080/creaPietanza/getTags", null).then(
+            function (response) {
+                $scope.tags = CreaPietanzaService.parseTagArray(response);
+            }
+            , function (response) {
+                alert("Couldn't get tags");
+            });
+    };
+
+    updateTagList();
 
     $scope.selectedProd = [];
     $scope.associatedTags = [];
     $scope.nomePietanza = "";
     $scope.nomeNewTag = "";
 
-     var searchIndex = function(searchTerm){
-         for(var i = 0, len = $scope.selectedProd.length; i < len; i++) {
-             if ($scope.selectedProd[i].name === searchTerm) {
-                 return i;
-             }
-         }
-     };
+    var searchIndex = function(searchTerm){
+        for(var i = 0, len = $scope.selectedProd.length; i < len; i++) {
+            if ($scope.selectedProd[i].name === searchTerm) {
+                return i;
+            }
+        }
+    };
 
     $scope.updateSelectedProd = function(nomeProd){
         var checkBox = document.getElementById("check.".concat(nomeProd));
@@ -70,13 +83,18 @@ myApp.controller("CreaPietanzaController", function($scope, ajaxService) {
         var jsonPiet = JSON.stringify(dtoPietanza);
         //CONTROLLA DEL URL SE PATH VA BENE
         ajaxService.sendResource("url", jsonPiet)
-    }
+    };
 
     $scope.saveTag = function(){
-        var dtoTag = {nome:$scope.nomeNewTag};
+        var dtoTag = {classificatore:$scope.nomeNewTag};
         var jsonTag = JSON.stringify(dtoTag);
         //CONTROLLA DEL URL SE PATH VA BENE
-        ajaxService.sendResource("url", jsonTag)
+        ajaxService.sendResource("http://localhost:8080/creaPietanza/addTag", jsonTag).then(function (response) {
+            updateTagList();
+        }, function (response) {
+            console.log(response)
+        });
+        $scope.nomeNewTag = "";
     }
 
 });
