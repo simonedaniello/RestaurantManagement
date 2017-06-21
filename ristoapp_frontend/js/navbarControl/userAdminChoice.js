18ac25e45040fbb07de5c348e7d8dc07abefb418
dandi <deliterjob@gmail.com>
2017-06-21 20:23:36 +0200

    simple commit on navbar

--- ristoapp_frontend/js/navbarControl/userAdminChoice.js
+++ ristoapp_frontend/js/navbarControl/userAdminChoice.js
@@ -3,7 +3,7 @@
  */
 
 var button = document.getElementById('useradmin');
-document.getElementById('amministratore').checked = true;
+document.getElementById('cuoco').checked = true;
 utenteadminfunzione();
 
 function utenteadminfunzione(){
@@ -18,14 +18,25 @@ function utenteadminfunzione(){
         document.getElementById('mostramenu').style.display = "block";
         document.getElementById('contattaci').style.display = "block";
         document.getElementById('dovesiamo').style.display = "block";
-    }else if(document.getElementById('amministratore').checked) {
-        it = 'Amministratore';
+    }else if(document.getElementById('cuoco').checked) {
+        it = 'Cuoco';
         button.innerText = button.textContent = it;
         document.getElementById('menu').style.display = "block";
         document.getElementById('creamenu').style.display = "block";
-        document.getElementById('prendicomanda').style.display = "block";
+        document.getElementById('prendicomanda').style.display = "none";
         document.getElementById('listacomandecuoco').style.display = "block";
         document.getElementById('pietanze').style.display = "block";
+        document.getElementById('mostramenu').style.display = "none";
+        document.getElementById('contattaci').style.display = "none";
+        document.getElementById('dovesiamo').style.display = "none";
+    }else if(document.getElementById('cameriere').checked) {
+        it = 'Cameriere';
+        button.innerText = button.textContent = it;
+        document.getElementById('menu').style.display = "none";
+        document.getElementById('creamenu').style.display = "none";
+        document.getElementById('prendicomanda').style.display = "block";
+        document.getElementById('listacomandecuoco').style.display = "none";
+        document.getElementById('pietanze').style.display = "none";
         document.getElementById('mostramenu').style.display = "none";
         document.getElementById('contattaci').style.display = "none";
         document.getElementById('dovesiamo').style.display = "none";
