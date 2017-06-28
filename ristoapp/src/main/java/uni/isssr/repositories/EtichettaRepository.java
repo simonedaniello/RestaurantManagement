ca6b059c491025f21ae5ea19f7c111292f010298
francesco <ripper2@live.it>
2017-06-28 17:51:24 +0200

    Fatto funzionare il caso d'uso crea pietanza facendolo comunicare completamente col backend, sia per l'invio ed il salvataggio dei dati che per la get delle pietanze

--- ristoapp/src/main/java/uni/isssr/repositories/EtichettaRepository.java
+++ ristoapp/src/main/java/uni/isssr/repositories/EtichettaRepository.java
@@ -2,7 +2,15 @@ package uni.isssr.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import uni.isssr.entities.Etichetta;
 
 public interface EtichettaRepository extends JpaRepository<Etichetta, String>{
+
+    @Transactional
+    @Modifying
+    @Query("delete from Etichetta where classificatore = ?1")
+    public void deleteEtichetta(String classificatore);
 }
--- ristoapp/src/main/java/uni/isssr/repositories/ResocontoRepository.java
+++ ristoapp/src/main/java/uni/isssr/repositories/ResocontoRepository.java
@@ -1,13 +1,29 @@
 package uni.isssr.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+import uni.isssr.dto.ResocontoAnalyticsDto;
+import uni.isssr.entities.Categoria;
 import uni.isssr.entities.ResocontoPietanza;
 import uni.isssr.utilities.IdResoconto;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by francesco on 19/06/17.
  */
 public interface ResocontoRepository extends JpaRepository<ResocontoPietanza, IdResoconto> {
+
+    /*@Transactional
+    @Query("select all " +
+            "from Menu as M join M.categorie as C join C.pietanze as P join Resoconto as R")
+    public List<String> getResocontiByData(String data);*/
+    /*,sum(R.venduto),sum(R.preparato)
+     +
+            + "group by C.nome +
+            " where M.isActive = true and M.categorie = C.id and C.pietanze = P.id and R.prodottoId = P.id" +
+            " and R.data = ?1 ""
+    * */
 }
