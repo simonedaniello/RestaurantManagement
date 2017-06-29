61e2494ecb4821730ef2f63f956af83d8dd195a5
alberto <comes94@gmail.com>
2017-06-29 19:22:29 +0200

    ricercaPietanza funzionante

--- ristoapp_frontend/js/controllers/ricercaPietanzaController.js
+++ ristoapp_frontend/js/controllers/ricercaPietanzaController.js
@@ -6,28 +6,42 @@ myApp.controller("ricercaPietanzaController", function ($scope, ajaxService, Ric
 
     var pageLimit = 5;
 
-    var getPietanzeList = function (pagina) {
-        ajaxService.getResource("http://localhost:8080/dish/pietanze?page=" + pagina.toString() + "&size=3", null).then(function (response) {
+    var getPietanzeListPage = function (url, pagina) {
+        ajaxService.getResource("http://localhost:8080/dish/pietanze", {page : pagina, size : 3}).then(function (response) {
             $scope.listaPietanze = response.content;
             $scope.currentPage = pagina;
-            $location.hash('main');
+            $location.hash('top');
             $anchorScroll();
-            var limit = $scope.currentPage + pageLimit;
-            var pagine = [];
-            if (limit < response.totalPages){
-                $scope.nextPage = false;
-                for (var i = $scope.currentPage; i < limit; i++) pagine.push(i);
-            }
-            else {
-                $scope.nextPage = true;
-                var i = $scope.currentPage - (limit -response.totalPages);
-                if (i < 0) i = 0;
-                for (; i < response.totalPages; i++) pagine.push(i);
-            }
-            $scope.listaPagine = pagine;
+            $scope.listaPagine = setPaginationNavbar(response);
         }, function (response) {
             console.log(response);
         });
+    };
+
+    var initPage = function () {
+        ajaxService.getResource("http://localhost:8080/dish/pietanze", {page : 0, size : 3}).then(function (response) {
+            $scope.listaPietanze = response.content;
+            $scope.currentPage = 0;
+            $scope.listaPagine = setPaginationNavbar(response);
+        }, function (response) {
+            console.log(response);
+        });
+    };
+
+    var setPaginationNavbar = function (response) {
+        var limit = $scope.currentPage + pageLimit;
+        var pagine = [];
+        if (limit < response.totalPages){
+            $scope.nextPage = false;
+            for (var i = $scope.currentPage; i < limit; i++) pagine.push(i);
+        }
+        else {
+            $scope.nextPage = true;
+            var i = $scope.currentPage - (limit -response.totalPages);
+            if (i < 0) i = 0;
+            for (; i < response.totalPages; i++) pagine.push(i);
+        }
+        return pagine;
     };
 
     var updateTagList = function() {
@@ -40,10 +54,18 @@ myApp.controller("ricercaPietanzaController", function ($scope, ajaxService, Ric
             });
     };
 
-    $scope.updatePietanzePagina = getPietanzeList;
+    var getPietanzeNavigation = function (pagina) {
+        getPietanzeListPage("http://localhost:8080/dish/pietanze", pagina);
+    };
+
+    var getPietanzeSearch = function (pagina) {
+        getPietanzeListPage("http://localhost:8080/dish", pagina);
+    };
+
+    $scope.updatePietanzePagina = getPietanzeNavigation;
 
     updateTagList();
-    getPietanzeList(0);
+    initPage();
 
 
     $scope.associatedTags = [];
@@ -59,11 +81,22 @@ myApp.controller("ricercaPietanzaController", function ($scope, ajaxService, Ric
     };
 
     $scope.deletePietanza = function (id) {
-        console.log("OK");
         ajaxService.deleteResource("http://localhost:8080/dish/delete/" + id.toString(), null).then(function (response) {
-            getPietanzeList();
+            initPage();
         }, function (response) {
             console.log(response);
         });
     };
+
+    $scope.ricerca = function () {
+        //var url = RicercaPietanzaService.generateSearchURL($scope.searchNome, $scope.associatedTags);
+        ajaxService.getResource("http://localhost:8080/dish", {page : 0, size : 3, nome : $scope.searchNome, tags : $scope.associatedTags}).then(function (response) {
+            $scope.listaPietanze = response.content;
+            $scope.currentPage = 0;
+            $scope.listaPagine = setPaginationNavbar(response);
+            $scope.updatePietanzePagina = getPietanzeSearch;
+        }, function (response) {
+            console.log(response);
+        })
+    }
 });
