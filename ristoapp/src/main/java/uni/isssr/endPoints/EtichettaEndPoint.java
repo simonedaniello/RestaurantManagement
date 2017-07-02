4b393b1bde40d49a74ab698b05a955a7ee1d72cc
francesco <ripper2@live.it>
2017-07-02 19:32:21 +0200

    inserito metodo put per fare update del tag

--- ristoapp/src/main/java/uni/isssr/endPoints/EtichettaEndPoint.java
+++ ristoapp/src/main/java/uni/isssr/endPoints/EtichettaEndPoint.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import uni.isssr.dto.EtichettaDto;
 import uni.isssr.entities.Etichetta;
 import uni.isssr.repositories.EtichettaRepository;
+import uni.isssr.repositories.PietanzaRepository;
+import uni.isssr.service.EtichettaService;
 
 import java.util.List;
 
@@ -16,10 +18,18 @@ public class EtichettaEndPoint {
     @Autowired
     private EtichettaRepository etichettaRepository;
 
+    @Autowired
+    private EtichettaService etichettaService;
+
     @RequestMapping(method = RequestMethod.POST)
     public void addTag(@RequestBody EtichettaDto etichettaDto){
         Etichetta etichetta = new Etichetta(etichettaDto.getClassificatore());
         etichettaRepository.save(etichetta);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/{ID}")
+    public void modifyTag(@RequestBody EtichettaDto etichettaDto, @PathVariable(value = "ID") String oldClassificatore){
+        etichettaService.updateEtichetta(etichettaDto.getClassificatore(),oldClassificatore);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{ID}")
