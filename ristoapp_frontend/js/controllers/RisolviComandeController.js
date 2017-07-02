3ff352b5788c27bf89054e44dc6d78b7ea8af2fc
dandi <deliterjob@gmail.com>
2017-07-02 12:49:31 +0200

    small fix p2

--- ristoapp_frontend/js/controllers/RisolviComandeController.js
+++ ristoapp_frontend/js/controllers/RisolviComandeController.js
@@ -1,6 +1,3 @@
-/**
- * Created by Antonio on 21/06/2017.
- */
 
 myApp.controller("RisolviComandeController", function ($scope,ajaxService) {
 
@@ -35,54 +32,5 @@ myApp.controller("RisolviComandeController", function ($scope,ajaxService) {
         $scope.selectedProdForChef.splice(indx,1);
     };
 
-
-    /*
-
-    // Chiavi di accesso al servizio PubNub
-    Pubnub.init({
-        publishKey: 'pub-c-18cb2897-1549-41e9-9011-c0c17480a1e4',
-        subscribeKey: 'sub-c-23206a28-56ae-11e7-97fe-02ee2ddab7fe'
-    });
-
-    $scope.subscribe = function () {
-        // Subscribe to a channel
-        Pubnub.addListener({
-            status: function(statusEvent) {
-                if (statusEvent.category === "PNUnknownCategory") {
-                    var newState = {new: 'error'};
-                    Pubnub.setState({state: newState},
-                        function (status) {
-                            console.log(status)
-                        }
-                    );
-                }
-            },
-
-            // Callback riguardante un evento che è stato appena pubblicato
-            message: function(response) {
-                var singleComanda = {comandaItems:[],         tavolo:0};
-                var comande = response.message;
-                var parsed = JSON.parse(comande);
-                for(var i in parsed.comandaItems){
-                    singleComanda.comandaItems.push(parsed.comandaItems[i]);
-                }
-                singleComanda.tavolo = parsed.tavolo;
-                $scope.currentItems.push(singleComanda);
-
-                $scope.selectedProdForChef = $scope.currentItems;
-
-                $scope.$apply();
-                //$scope.selectedProdForChef = comande;
-                //$scope.$apply();
-            }
-        });
-
-        Pubnub.subscribe({
-            channels: ['channel_comande']
-        });
-        alert("SUBSCRIBED!");
-    };
-
-    */
 
 });
